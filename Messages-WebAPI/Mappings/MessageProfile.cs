2df17777ef4100bcfa8f2c8ea33f89f12d87e33e
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 13:05:35 +0200

    Add mapping for Message

---
+++ Messages-WebAPI/Mappings/MessageProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Messages_DAL.DTO.Messages;
+using Messages_DAL.Models;
+
+namespace Messages_WebAPI.Mappings
+{
+    public class MessageProfile : Profile
+    {
+        public MessageProfile()
+        {
+            CreateMap<Message, MessageDTO>();
+        }
+    }
+}
