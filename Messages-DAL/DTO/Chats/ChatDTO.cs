a7f105baf98ff1e3bb79c557740ae9bbda77891e
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 11:10:07 +0200

    Add ChatDTO

---
+++ Messages-DAL/DTO/Chats/ChatDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages_DAL.DTO.Chats
+{
+    public class ChatDTO
+    {
+        public int Id { get; set; }
+        public string Guid { get; set; }
+        public string Name { get; set; }
+    }
+}
