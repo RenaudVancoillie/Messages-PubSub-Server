a854d078df0bc179b36a4399269779c6c765d793
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 10:56:02 +0200

    Add CreateDatabaseSchema Migration

---
+++ Messages-DAL/Migrations/20210512085502_CreateDatabaseSchema.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Messages_DAL.Migrations
+{
+    public partial class CreateDatabaseSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chats",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    guid = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false, defaultValueSql: "(newid())"),
+                    name = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_chats", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    chat_id = table.Column<int>(type: "int", nullable: false),
+                    sender = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: false),
+                    text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())"),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_messages_chats",
+                        column: x => x.chat_id,
+                        principalTable: "chats",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__chats__497F6CB52BDA7CF9",
+                table: "chats",
+                column: "guid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_chat_id",
+                table: "messages",
+                column: "chat_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "chats");
+        }
+    }
+}
