debe9be52693a54306c8be64d7715e1f59c49988
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 13:03:39 +0200

    Add MessageDTO

---
+++ Messages-DAL/DTO/Messages/MessageDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages_DAL.DTO.Messages
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+        public int ChatId { get; set; }
+        public string Sender { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
