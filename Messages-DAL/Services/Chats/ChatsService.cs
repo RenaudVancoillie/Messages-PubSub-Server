16f26738991d9658f39a043698df12457528775b
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-12 11:11:54 +0200

    Use ChatDTO

--- Messages-DAL/Services/Chats/ChatsService.cs
+++ Messages-DAL/Services/Chats/ChatsService.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Messages_DAL.DTO.Chats;
 using Messages_DAL.Models;
 using Messages_DAL.Repositories.Chats;
 
 namespace Messages_DAL.Services.Chats
 {
-    public class ChatService : IChatsService
+    public class ChatsService : IChatsService
     {
         private readonly IChatsRepository chatsRepository;
 
-        public ChatService(IChatsRepository chatsRepository)
+        public ChatsService(IChatsRepository chatsRepository)
         {
             this.chatsRepository = chatsRepository;
         }
 
-        public IEnumerable<Chat> GetAll() => chatsRepository.GetAll();
+        public IEnumerable<ChatDTO> GetAll()
+        {
+            return chatsRepository.GetAll()
+                .Select(c => new ChatDTO
+                {
+                    Id = c.Id,
+                    Guid = c.Guid,
+                    Name = c.Name
+                });
+        }
     }
 }
--- Messages-DAL/Services/Chats/IChatsService.cs
+++ Messages-DAL/Services/Chats/IChatsService.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Messages_DAL.Models;
+using Messages_DAL.DTO.Chats;
 
 namespace Messages_DAL.Services.Chats
 {
     public interface IChatsService
     {
-        IEnumerable<Chat> GetAll();
+        IEnumerable<ChatDTO> GetAll();
     }
 }
